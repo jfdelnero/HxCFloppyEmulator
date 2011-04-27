7a5c660a96ce7d400322b9c14f273d52421c101a
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-04-27 21:15:34 +0000

    MacOsX : remove of malloc.h
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@80 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- common/mfm_file_writer.c
+++ common/mfm_file_writer.c
@@ -24,7 +24,7 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 */
-#include <malloc.h>
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -68,7 +68,7 @@ int write_MFM_file(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppy,char * filen
 		}
 
 		mfmheader.floppyRPM=0;//floppy->floppyRPM;
-		mfmheader.floppyiftype=floppy->floppyiftype;
+		mfmheader.floppyiftype=(unsigned char)floppy->floppyiftype;
 		mfmheader.mfmtracklistoffset=sizeof(mfmheader);
 		fwrite(&mfmheader,sizeof(mfmheader),1,hxcmfmfile);
 		
