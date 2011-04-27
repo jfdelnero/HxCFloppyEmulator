3bc55dcf36b34da51df32535cbe48a31cfc4a62a
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-04-27 21:02:18 +0000

    Use of the new track generator.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@65 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- common/plugins/vdk_loader/vdk_loader.c
+++ common/plugins/vdk_loader/vdk_loader.c
@@ -112,16 +112,14 @@ int VDK_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydisk,ch
 {
 	
 	FILE * f;
-	unsigned int filesize,sectorsize,filetracklen;
-	unsigned int i,j;
-	unsigned int file_offset,gap3len,interleave;
-	char* trackdata;
+	unsigned int filesize,filetracklen;
+	unsigned int i,j,file_offset;
+	unsigned char  gap3len,interleave;
+	unsigned char* trackdata;
 	vdk_header vdk_h;
-	int rpm;
-	int tracklen;
+	unsigned short rpm,sectorsize;
+	unsigned char skew,trackformat;
 	CYLINDER* currentcylinder;
-	SIDE* currentside;
-	
 	
 	floppycontext->hxc_printf(MSG_DEBUG,"VDK_libLoad_DiskFile %s",imgfile);
 	
@@ -158,15 +156,15 @@ int VDK_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydisk,ch
 	floppydisk->tracks=(CYLINDER**)malloc(sizeof(CYLINDER*)*floppydisk->floppyNumberOfTrack);
 			
 	rpm=300; // normal rpm
-	
+	skew=0;
 	floppycontext->hxc_printf(MSG_INFO_1,"VDK File : VDK version: 0x%.2x,Source ID: 0x%.2x, Source Version: 0x%.2x, Flags: 0x%.2x",vdk_h.version,vdk_h.file_source_id,vdk_h.file_source_ver,vdk_h.flags);			
 	floppycontext->hxc_printf(MSG_INFO_1,"%d tracks, %d side(s), %d sectors/track, rpm:%d",floppydisk->floppyNumberOfTrack,floppydisk->floppyNumberOfSide,floppydisk->floppySectorPerTrack,rpm);
 							
 	sectorsize=256;
 	gap3len=24;
 	interleave=2;
+	trackformat=ISOFORMAT_DD;
 
-	tracklen=(DEFAULT_DD_BITRATE/(rpm/60))/4;
 	filetracklen=sectorsize*floppydisk->floppySectorPerTrack;
 	trackdata=(unsigned char*)malloc(filetracklen);
 
@@ -174,47 +172,17 @@ int VDK_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydisk,ch
 	for(j=0;j<floppydisk->floppyNumberOfTrack;j++)
 	{
 				
-		floppydisk->tracks[j]=(CYLINDER*)malloc(sizeof(CYLINDER));
+		floppydisk->tracks[j]=allocCylinderEntry(rpm,floppydisk->floppyNumberOfSide);
 		currentcylinder=floppydisk->tracks[j];
-		currentcylinder->number_of_side=floppydisk->floppyNumberOfSide;
-		currentcylinder->sides=(SIDE**)malloc(sizeof(SIDE*)*currentcylinder->number_of_side);
-		memset(currentcylinder->sides,0,sizeof(SIDE*)*currentcylinder->number_of_side);
 				
 		for(i=0;i<floppydisk->floppyNumberOfSide;i++)
 		{
-					
-			currentcylinder->floppyRPM=rpm;
-					
-			currentcylinder->sides[i]=malloc(sizeof(SIDE));
-			memset(currentcylinder->sides[i],0,sizeof(SIDE));
-			currentside=currentcylinder->sides[i];
-					
-			currentside->number_of_sector=floppydisk->floppySectorPerTrack;
-			currentside->tracklen=tracklen;
-
-			currentside->databuffer=malloc(currentside->tracklen);
-			memset(currentside->databuffer,0,currentside->tracklen);
-					
-			currentside->flakybitsbuffer=0;
-				
-			currentside->timingbuffer=0;
-			currentside->bitrate=DEFAULT_DD_BITRATE;
-			currentside->track_encoding=ISOIBM_MFM_ENCODING;
-
-			currentside->indexbuffer=malloc(currentside->tracklen);
-			memset(currentside->indexbuffer,0,currentside->tracklen);						
-
 			file_offset=((filetracklen*j*floppydisk->floppyNumberOfSide)+(filetracklen*(i&1)))+vdk_h.header_size;					
 			fseek (f , file_offset , SEEK_SET);
 					
 			fread(trackdata,filetracklen,1,f);
 
-			BuildISOTrack(floppycontext,ISOFORMAT_DD,currentside->number_of_sector,1,sectorsize,j,i,gap3len,trackdata,currentside->databuffer,&currentside->tracklen,interleave,0,NULL);			
-
-			currentside->tracklen=currentside->tracklen*8;
-
-			fillindex(currentside->tracklen-1,currentside,2500,TRUE,1);
-
+			currentcylinder->sides[i]=tg_generatetrack(trackdata,sectorsize,floppydisk->floppySectorPerTrack,(unsigned char)j,(unsigned char)i,1,interleave,(unsigned char)(((j<<1)|(i&1))*skew),floppydisk->floppyBitRate,currentcylinder->floppyRPM,trackformat,gap3len,2500);
 		}
 
 	}
