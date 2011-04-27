57fc5b03680157b772943ad6851253ce806fee50
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-04-27 21:18:01 +0000

    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@85 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- linux/hxcfloppyemu.c
+++ linux/hxcfloppyemu.c
@@ -25,7 +25,6 @@
 //
 */
 #include <errno.h>
-#include <malloc.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
--- linux/hxcfloppyemu_convert.c
+++ linux/hxcfloppyemu_convert.c
@@ -25,13 +25,12 @@
 //
 */
 #include <errno.h>
-#include <malloc.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdarg.h> 
-#include <sched.h> 
+#include <stdarg.h>
+#include <sched.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
