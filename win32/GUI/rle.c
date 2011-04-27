95af6fd05fcbc1ac2e379c09520ae00258f447ad
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-04-27 21:19:59 +0000

    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@89 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- win32/GUI/rle.c
+++ win32/GUI/rle.c
@@ -24,7 +24,7 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 */
-#include <malloc.h>
+#include <stdlib.h>
 #include <string.h> 
 
 void rlepack(unsigned char * bufferin,int sizein,unsigned char * bufferout,int * sizeout)
