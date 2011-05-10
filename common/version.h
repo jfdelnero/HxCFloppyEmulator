ad94f514b46712d1564d41a616c92e38ab25da7f
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-05-10 22:25:01 +0000

    FDI file format support.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@147 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- common/version.h
+++ common/version.h
@@ -1,7 +1,7 @@
 #define FILE_VERSION        1,6,5,3
-#define STR_FILE_VERSION   "1,6,5,3a\0"
-#define STR_FILE_VERSION2  "1.6.5.3a"
-#define STR_DATE           "09/05/2011\0"
+#define STR_FILE_VERSION   "1,6,5,3b\0"
+#define STR_FILE_VERSION2  "1.6.5.3b"
+#define STR_DATE           "10/05/2011\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
