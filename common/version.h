52e8f52ccdc41b22d39626c2d566c7c9ff16b593
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-05-14 11:13:16 +0000

    Acorn BBC SSD/DSD/ADL file format support.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@154 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- common/version.h
+++ common/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        1,6,6,0
-#define STR_FILE_VERSION   "1,6,6,0a\0"
-#define STR_FILE_VERSION2  "1.6.6.0a"
-#define STR_DATE           "11/05/2011\0"
+#define FILE_VERSION        1,6,7,0
+#define STR_FILE_VERSION   "1,6,7,0a\0"
+#define STR_FILE_VERSION2  "1.6.7.0a"
+#define STR_DATE           "14/05/2011\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
