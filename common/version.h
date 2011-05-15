e9830f4cf4976fe7acbd5ed75757152d1f3cc35e
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-05-15 06:26:38 +0000

    Dump Retry parameter, dump progression graphic.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@160 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- common/version.h
+++ common/version.h
@@ -1,6 +1,6 @@
-#define FILE_VERSION        1,6,7,0
-#define STR_FILE_VERSION   "1,6,7,0a\0"
-#define STR_FILE_VERSION2  "1.6.7.0a"
+#define FILE_VERSION        1,6,8,0
+#define STR_FILE_VERSION   "1,6,8,0a\0"
+#define STR_FILE_VERSION2  "1.6.8.0a"
 #define STR_DATE           "14/05/2011\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
