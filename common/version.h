2e01e38bd8b5aebacc9a646d105703e10cd8c093
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-08-14 08:17:59 +0000

    TI99 V9T9 support : Sector id corrected!
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@208 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- common/version.h
+++ common/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        1,6,11,0a
-#define STR_FILE_VERSION   "1,6,11,0a\0"
-#define STR_FILE_VERSION2  "1.6.11.0a"
-#define STR_DATE           "13/08/2011\0"
+#define FILE_VERSION        1,6,11,1a
+#define STR_FILE_VERSION   "1,6,11,1a\0"
+#define STR_FILE_VERSION2  "1.6.11.1a"
+#define STR_DATE           "14/08/2011\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
