823033c21193e0549d3b09e9e073f88281094401
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-09-24 11:10:44 +0000

    Ensoniq GKH file support, zlib 1.2.5.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@236 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- common/version.h
+++ common/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        1,6,11,9
-#define STR_FILE_VERSION   "1,6,11,9a\0"
-#define STR_FILE_VERSION2  "1.6.11.9a"
-#define STR_DATE           "20/09/2011\0"
+#define FILE_VERSION        1,6,11,11
+#define STR_FILE_VERSION   "1,6,11,11a\0"
+#define STR_FILE_VERSION2  "1.6.11.11a"
+#define STR_DATE           "24/09/2011\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
