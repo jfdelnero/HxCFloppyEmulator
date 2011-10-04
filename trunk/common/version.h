eaeb5c206225fea574747c1c24a7d751952a8adb
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-10-04 19:43:36 +0000

    IMZ gap3 corrected.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software@246 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- trunk/common/version.h
+++ trunk/common/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        1,6,11,14
-#define STR_FILE_VERSION   "1,6,11,14a\0"
-#define STR_FILE_VERSION2  "1.6.11.14a"
-#define STR_DATE           "26/09/2011\0"
+#define FILE_VERSION        1,6,11,15
+#define STR_FILE_VERSION   "1,6,11,15a\0"
+#define STR_FILE_VERSION2  "1.6.11.15a"
+#define STR_DATE           "04/10/2011\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
