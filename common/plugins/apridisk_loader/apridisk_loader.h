ce5ca5d960661006428fb3e8c3e472eb1964294e
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-12-03 18:58:45 +0000

    Major code refactoring : preparing the version 2.X.X.X.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@258 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- common/plugins/apridisk_loader/apridisk_loader.h
+++ common/plugins/apridisk_loader/apridisk_loader.h
@@ -24,9 +24,5 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 */
-int ApriDisk_libIsValidDiskFile(HXCFLOPPYEMULATOR* floppycontext,char * imgfile);
-int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydisk,char * imgfile,void * parameters);
 
-
-
-
+int ApriDisk_libGetPluginInfo(HXCFLOPPYEMULATOR* floppycontext,unsigned long infotype,void * returnvalue);
