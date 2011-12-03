09dbb02d056252c70b594278dd6ba696d8aedafa
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2011-12-03 18:58:45 +0000

    Major code refactoring : preparing the version 2.X.X.X.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software@258 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- trunk/common/plugins/apridisk_loader/apridisk_loader.c
+++ trunk/common/plugins/apridisk_loader/apridisk_loader.c
@@ -49,12 +49,12 @@
 
 #include "types.h"
 #include "hxc_floppy_emulator.h"
-#include "internal_floppy.h"
+
 #include "floppy_loader.h"
 #include "floppy_utils.h"
 
 #include "../common/crc.h"
-#include "../common/track_generator.h"
+
 
 #include "apridisk_loader.h"
 #include "apridisk_format.h"
@@ -82,18 +82,18 @@ int ApriDisk_libIsValidDiskFile(HXCFLOPPYEMULATOR* floppycontext,char * imgfile)
 				if(!strcmp(APRIDISK_HeaderString,HeaderBuffer))
 				{
 					floppycontext->hxc_printf(MSG_DEBUG,"ApriDisk file !");
-					return LOADER_ISVALID;
+					return HXCFE_VALIDFILE;
 				}
 				else
 				{
 					floppycontext->hxc_printf(MSG_DEBUG,"ApriDisk file !");
-					return LOADER_BADFILE;
+					return HXCFE_BADFILE;
 				}
 			}
 		}
 	}
 
-	return LOADER_BADPARAMETER;
+	return HXCFE_BADPARAMETER;
 }
 
 
@@ -121,7 +121,7 @@ int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydi
 	if(f==NULL) 
 	{
 		floppycontext->hxc_printf(MSG_ERROR,"Cannot open %s !",imgfile);
-		return LOADER_ACCESSERROR;
+		return HXCFE_ACCESSERROR;
 	}
 	
 	fseek(f,0,SEEK_END);
@@ -137,12 +137,12 @@ int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydi
 	if(strcmp(APRIDISK_HeaderString,&file_buffer[fileindex]))
 	{
 		floppycontext->hxc_printf(MSG_DEBUG,"ApriDisk file !");
-		return LOADER_BADFILE;
+		return HXCFE_BADFILE;
 	}
 	
 	number_of_track=0;
 	i=0;
-	
+
 	floppydisk->floppyBitRate=500000;
 	floppydisk->floppyiftype=GENERIC_SHUGART_DD_FLOPPYMODE;
 	floppydisk->floppyNumberOfTrack=0;
@@ -184,68 +184,68 @@ int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydi
 					}
 					if((data_record->cylinder==i) && (data_record->head==j))
 					{
-						
+
 						if((j+1)>(floppydisk->floppyNumberOfSide))
 						{
 							floppydisk->floppyNumberOfSide=j+1;
 						}
-						
+
 						if((i+1)>(floppydisk->floppyNumberOfTrack))
 						{
 							floppydisk->floppyNumberOfTrack=i+1;
 							newtrack=1;
 						}
-						
+
 						data_record->item_type=DATA_RECORD_DELETED;
-						
-						
+
+
 						sectorconfig=(SECTORCONFIG*)realloc(sectorconfig,sizeof(SECTORCONFIG)*(number_of_sector+1));
 						memset(&sectorconfig[number_of_sector],0,sizeof(SECTORCONFIG));
-						
+
 						sectorconfig[number_of_sector].cylinder=i;
 						sectorconfig[number_of_sector].head=j;
 						sectorconfig[number_of_sector].sector=data_record->sector;
 						sectorconfig[number_of_sector].trackencoding=IBMFORMAT_DD;
 						sectorconfig[number_of_sector].bitrate=floppydisk->floppyBitRate;
 						sectorconfig[number_of_sector].gap3=255;
-						
+
 						switch(data_record->compression)
 						{
 						case DATA_NOT_COMPRESSED:
-							sectorconfig[number_of_sector].sectorsize=data_record->data_size;							
+							sectorconfig[number_of_sector].sectorsize=data_record->data_size;
 							sectorconfig[number_of_sector].input_data=malloc(data_record->data_size);
 							memcpy(sectorconfig[number_of_sector].input_data,&file_buffer[fileindex],data_record->data_size);
 							fileindex=fileindex+data_record->data_size;
 							break;
-							
+
 						case DATA_COMPRESSED:
 							compressed_dataitem=(apridisk_compressed_data *)&file_buffer[fileindex];
-							
+
 							sectorconfig[number_of_sector].sectorsize=compressed_dataitem->count;
 							sectorconfig[number_of_sector].input_data=malloc(compressed_dataitem->count);
 
 							memset(sectorconfig[number_of_sector].input_data,compressed_dataitem->byte,compressed_dataitem->count);
 							fileindex=fileindex+data_record->data_size;
-							
+
 							break;
-							
+
 						default:
 							floppycontext->hxc_printf(MSG_ERROR,"Unknow compression id (%.4x) !",data_record->compression);
 							sectorconfig[number_of_sector].input_data=0;
 							break;
 						}
-					
+
 						number_of_sector++;
-						
+
 					}
 					else
 					{
 						fileindex=fileindex+data_record->data_size;
 					}
-					
+
 					floppycontext->hxc_printf(MSG_DEBUG,"ApriDisk_libLoad_DiskFile: item DATA_RECORD_SECTOR found. Header size=%d, Data size=%d, Sector=%d Head=%d Cylinder=%d",data_record->header_size,data_record->data_size,data_record->sector,data_record->head,data_record->cylinder);
 					break;
-					
+
 				case DATA_RECORD_COMMENT:
 					if(data_record->header_size>sizeof(apridisk_data_record))
 					{
@@ -253,22 +253,22 @@ int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydi
 					}
 					fileindex=fileindex+data_record->data_size;
 					break;
-					
+
 				case DATA_RECORD_CREATOR:
-					
+
 					if(data_record->header_size>sizeof(apridisk_data_record))
 					{
 						fileindex=fileindex+(data_record->header_size-sizeof(apridisk_data_record));
 					}
-					fileindex=fileindex+data_record->data_size;					
+					fileindex=fileindex+data_record->data_size;
 					break;
 
 				default:
-					return LOADER_BADFILE;
+					return HXCFE_BADFILE;
 					break;
-					
+
 			}
-			
+
 		}while(fileindex<totalfilesize);
 
 		if(newtrack)
@@ -282,7 +282,7 @@ int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydi
 		currentcylinder->number_of_side=floppydisk->floppyNumberOfSide;
 		currentcylinder->sides=(SIDE**)realloc(currentcylinder->sides,sizeof(SIDE*)*currentcylinder->number_of_side);
 		//memset(currentcylinder->sides,0,sizeof(SIDE*)*currentcylinder->number_of_side);
-		
+
 		currentcylinder->floppyRPM=rpm;
 
 		currentside=tg_generatetrackEx((unsigned short)number_of_sector,sectorconfig,interleave,0,floppydisk->floppyBitRate,rpm,IBMFORMAT_DD,2500 | NO_SECTOR_UNDER_INDEX,-2500);
@@ -298,19 +298,19 @@ int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydi
 				free(sectorconfig);
 
 		}
-		
+
 		number_of_sector=0;
 	}
-	
+
 
 	// Comment & creator extraction.
 	fileindex=128;
 	do
 	{
-				
+
 		data_record=(apridisk_data_record *)&file_buffer[fileindex];
 		fileindex=fileindex+sizeof(apridisk_data_record);
-				
+
 		switch(data_record->item_type)
 		{
 			case DATA_RECORD_DELETED:
@@ -320,7 +320,7 @@ int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydi
 				}
 				fileindex=fileindex+(data_record->data_size);
 				break;
-					
+
 			case DATA_RECORD_SECTOR:
 				if(data_record->header_size>sizeof(apridisk_data_record))
 				{
@@ -328,7 +328,7 @@ int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydi
 				}
 				fileindex=fileindex+data_record->data_size;
 				break;
-					
+
 			case DATA_RECORD_COMMENT:
 				if(data_record->header_size>sizeof(apridisk_data_record))
 				{
@@ -337,7 +337,7 @@ int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydi
 				floppycontext->hxc_printf(MSG_DEBUG,"ApriDisk_libLoad_DiskFile: item DATA_RECORD_COMMENT found: %s",&file_buffer[fileindex]);
 				fileindex=fileindex+data_record->data_size;
 				break;
-					
+
 			case DATA_RECORD_CREATOR:
 				if(data_record->header_size>sizeof(apridisk_data_record))
 				{
@@ -348,19 +348,45 @@ int ApriDisk_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydi
 				break;
 
 				default:
-				return LOADER_BADFILE;
+				return HXCFE_BADFILE;
 				break;
-					
+
 			}
-			
+
 		}while(fileindex<totalfilesize);
 
 
 
 	free(file_buffer);
-	
-	
-	
-	return LOADER_NOERROR;
+
+
+
+	return HXCFE_NOERROR;
+}
+
+int ApriDisk_libGetPluginInfo(HXCFLOPPYEMULATOR* floppycontext,unsigned long infotype,void * returnvalue)
+{
+
+	const char plug_id[]="APRIDISK";
+	const char plug_desc[]="APRIDISK Loader";
+	const char plug_ext[]="dsk";
+
+	plugins_ptr plug_funcs=
+	{
+		(ISVALIDDISKFILE)	ApriDisk_libIsValidDiskFile,
+		(LOADDISKFILE)		ApriDisk_libLoad_DiskFile,
+		(WRITEDISKFILE)		0,
+		(GETPLUGININFOS)	ApriDisk_libGetPluginInfo
+	};
+
+	return libGetPluginInfo(
+			floppycontext,
+			infotype,
+			returnvalue,
+			plug_id,
+			plug_desc,
+			&plug_funcs,
+			plug_ext
+			);
 }
 
