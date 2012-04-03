41f6ac8a75b816ee64d551cae2c6577fd3d86b5b
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2012-04-03 20:15:17 +0000

    Akai S950 DD & Akai S950 HD interface mode support added (need firmware v1.6.4.4 or more)
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/tags/HxCFloppyEmulator_software_V1_6_11_16@354 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- common/version.h
+++ common/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        1,6,11,15
-#define STR_FILE_VERSION   "1,6,11,15\0"
-#define STR_FILE_VERSION2  "1.6.11.15"
-#define STR_DATE           "09/10/2011\0"
+#define FILE_VERSION        1,6,11,16
+#define STR_FILE_VERSION   "1,6,11,16\0"
+#define STR_FILE_VERSION2  "1.6.11.16"
+#define STR_DATE           "04/03/2012\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
