95a7a1ed2a7c6b3fabc04d7f12069680de6e75d5
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2012-04-17 22:31:45 +0000

    USB Floppy emulator lib.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libusbhxcfe/trunk@368 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

---
+++ libusbhxcfe/sources/version.h
@@ -0,0 +1,10 @@
+#define FILE_VERSION        2,0,0,0
+#define STR_FILE_VERSION   "2,0,0,0\0"
+#define STR_FILE_VERSION2  "2.0.0.0"
+#define STR_DATE           "04/01/2012\0"
+#define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
+
+#define FILEVER        FILE_VERSION
+#define PRODUCTVER     FILE_VERSION
+#define STRFILEVER     STR_FILE_VERSION
+#define STRPRODUCTVER  STR_FILE_VERSION
