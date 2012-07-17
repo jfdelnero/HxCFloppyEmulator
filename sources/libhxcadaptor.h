562eb5ed6c53171a050f87e363de123e9133e9d2
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2012-07-17 10:15:06 +0000

    OS "Glue" functions lib: Remove OS specific functions from lib & software.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcadaptor/trunk@447 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/libhxcadaptor.h
+++ sources/libhxcadaptor.h
@@ -1,3 +1,4 @@
+/////////////// Thread functions ////////////////
 
 typedef int (*THREADFUNCTION) (void* floppyemulator,void* hwemulator);
 
@@ -14,3 +15,33 @@ typedef struct filefoundinfo_
 	char filename[256];
 	int size;
 }filefoundinfo;
+
+int hxc_setevent(HXCFLOPPYEMULATOR* floppycontext,unsigned char id);
+unsigned long hxc_createevent(HXCFLOPPYEMULATOR* floppycontext,unsigned char id);
+int hxc_waitevent(HXCFLOPPYEMULATOR* floppycontext,int id,int timeout);
+void hxc_pause(int ms);
+int hxc_createthread(HXCFLOPPYEMULATOR* floppycontext,void* hwcontext,THREADFUNCTION thread,int priority);
+
+/////////////// String functions ///////////////
+
+#ifndef WIN32
+//void strlwr(char *string)
+#endif
+char * strupper(char * str);
+char * strlower(char * str);
+
+/////////////// File functions ////////////////
+
+int hxc_open (const char *filename, int flags, ...);
+
+FILE *hxc_fopen (const char *filename, const char *mode);
+int hxc_fclose(FILE * f);
+
+#ifndef stat
+#include <sys/stat.h>
+#endif
+int hxc_stat( const char *filename, struct stat *buf);
+
+long find_first_file(char *folder,char *file,filefoundinfo* fileinfo);
+long find_next_file(long handleff,char *folder,char *file,filefoundinfo* fileinfo);
+long find_close(long handle);
