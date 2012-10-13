359ce4a5faff232b91b3e507260d03564c1510f3
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2012-10-13 13:46:05 +0000

    Some code optimizations.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software@589 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- trunk/sources/gui/microintro/microintro.h
+++ trunk/sources/gui/microintro/microintro.h
@@ -4,20 +4,27 @@ typedef struct uintro_context_
 	unsigned short xsize;
 	unsigned short ysize;
 
-
 	unsigned short sprite_xsize;
 	unsigned short sprite_ysize;
 
 	unsigned int * framebuffer;
 	unsigned int * background;
 	unsigned int * blurbuffer;
+
 	float f1,f2;
+
+	float col_f1,col_f2,col_f3;
+	float col_f1s,col_f2s,col_f3s;
+
+	float sprt_f1,sprt_f2;
+
 	unsigned long tick;
 	unsigned char part;
 }uintro_context;
 
 
 uintro_context * uintro_init(unsigned short xsize,unsigned short ysize);
+void uintro_reset(uintro_context * ui_context);
 void uintro_getnextframe(uintro_context * democontext);
 void uintro_getnext_soundsample(uintro_context * democontext,unsigned char* buffer,int size);
 void uintro_deinit(uintro_context * democontext);
