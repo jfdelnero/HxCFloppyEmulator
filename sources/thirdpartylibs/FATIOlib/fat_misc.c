c602cab5f8225df2d530f715caa88ac644ad6f71
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2012-10-25 21:30:25 +0000

    Default file date corrected.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@609 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/thirdpartylibs/FATIOlib/fat_misc.c
+++ sources/thirdpartylibs/FATIOlib/fat_misc.c
@@ -253,21 +253,27 @@ void fatfs_filename_to_lfn(char *filename, uint8 *buffer, int entry, uint8 sfnCh
 void fatfs_sfn_create_entry(char *shortfilename, uint32 size, uint32 startCluster, struct fat_dir_entry *entry, int dir)
 {
     int i;
-
+    int year,month,mday;
+    unsigned short date; 
     // Copy short filename
     for (i=0;i<FAT_SFN_SIZE_FULL;i++)
         entry->Name[i] = shortfilename[i];
 
+    year = 2012 - 1900;
+    month= 9;
+    mday = 15;
+
+    date = (((year-80) &0x7F)<<9) | ((month+1 &0xF)<<5) | (mday &0x1F);
     // Unless we have a RTC we might as well set these to 1980
     entry->CrtTimeTenth = 0x00;
     entry->CrtTime[1] = entry->CrtTime[0] = 0x00;
-    entry->CrtDate[1] = 0x00;
-    entry->CrtDate[0] = 0x20;
-    entry->LstAccDate[1] = 0x00;
-    entry->LstAccDate[0] = 0x20;
+    entry->CrtDate[0] = date&0xFF;
+    entry->CrtDate[1] = (date>>8)&0xFF;
+    entry->LstAccDate[0] = date&0xFF;
+    entry->LstAccDate[1] = (date>>8)&0xFF;
     entry->WrtTime[1] = entry->WrtTime[0] = 0x00;
-    entry->WrtDate[1] = 0x00;
-    entry->WrtDate[0] = 0x20;    
+    entry->WrtDate[0] = date&0xFF;
+    entry->WrtDate[1] = (date>>8)&0xFF;
 
     if (!dir)
         entry->Attr = FILE_TYPE_FILE;
