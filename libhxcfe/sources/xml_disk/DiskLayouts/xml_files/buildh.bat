44e38236705e9ca37c51e694aa70839f2e16ae00
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2013-04-02 20:12:39 +0000

    New disk layouts : Special dos format & Roland disks.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@776 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- libhxcfe/sources/xml_disk/DiskLayouts/xml_files/buildh.bat
+++ libhxcfe/sources/xml_disk/DiskLayouts/xml_files/buildh.bat
@@ -7,6 +7,12 @@
 ..\..\converttools\bmptoh DiskLayout_AmstradCPC_DD.xml -DATA
 ..\..\converttools\bmptoh DiskLayout_Ensoniq_DD_800KB.xml -DATA
 ..\..\converttools\bmptoh DiskLayout_Ensoniq_HD_1600KB.xml -DATA
+..\..\converttools\bmptoh DiskLayout_DOS_ED_2_88MB.xml -DATA
+..\..\converttools\bmptoh DiskLayout_DOS_EXHD_6_78MB.xml -DATA
+..\..\converttools\bmptoh DiskLayout_DOS_EXHD_4_5MB.xml -DATA
+..\..\converttools\bmptoh DiskLayout_DOS_EXDD_2_5MB.xml -DATA
+..\..\converttools\bmptoh DiskLayout_Roland_W30_S330_W50_S50_S550_DD_720KB.xml -DATA
+
 move *.h ..
 
 
