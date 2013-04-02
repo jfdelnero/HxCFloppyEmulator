e9b2c3fb6768b6871386dd2a5a7454e95ded320e
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2013-04-02 20:12:39 +0000

    New disk layouts : Special dos format & Roland disks.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@776 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/xml_disk/DiskLayouts/LayoutsIndex.h
+++ sources/xml_disk/DiskLayouts/LayoutsIndex.h
@@ -7,6 +7,11 @@
 #include "data_DiskLayout_AmstradCPC_DD_xml.h"
 #include "data_DiskLayout_Ensoniq_DD_800KB_xml.h"
 #include "data_DiskLayout_Ensoniq_HD_1600KB_xml.h"
+#include "data_DiskLayout_DOS_ED_2_88MB_xml.h"
+#include "data_DiskLayout_DOS_EXDD_2_5MB_xml.h"
+#include "data_DiskLayout_DOS_EXHD_4_5MB_xml.h"
+#include "data_DiskLayout_DOS_EXHD_6_78MB_xml.h"
+#include "data_DiskLayout_Roland_W30_S330_W50_S50_S550_DD_720KB_xml.h"
 
 datatype * disklayout_list[]=
 {
@@ -19,6 +24,11 @@ datatype * disklayout_list[]=
 	data_DiskLayout_AmstradCPC_DD_xml,
 	data_DiskLayout_Ensoniq_DD_800KB_xml,
 	data_DiskLayout_Ensoniq_HD_1600KB_xml,
+	data_DiskLayout_Roland_W30_S330_W50_S50_S550_DD_720KB_xml,
+	data_DiskLayout_DOS_ED_2_88MB_xml,
+	data_DiskLayout_DOS_EXDD_2_5MB_xml,
+	data_DiskLayout_DOS_EXHD_4_5MB_xml,
+	data_DiskLayout_DOS_EXHD_6_78MB_xml,
 	0
 };
 
