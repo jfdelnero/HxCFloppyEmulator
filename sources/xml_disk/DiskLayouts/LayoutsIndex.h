4bab4a4093ad771402c54b68f238e6a81496d40d
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2013-04-10 20:22:54 +0000

    Linn / Forat 9000 raw image format.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@790 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/xml_disk/DiskLayouts/LayoutsIndex.h
+++ sources/xml_disk/DiskLayouts/LayoutsIndex.h
@@ -12,6 +12,7 @@
 #include "data_DiskLayout_DOS_EXHD_4_5MB_xml.h"
 #include "data_DiskLayout_DOS_EXHD_6_78MB_xml.h"
 #include "data_DiskLayout_Roland_W30_S330_W50_S50_S550_DD_720KB_xml.h"
+#include "data_DiskLayout_LinnForat9K_720KB_xml.h"
 
 datatype * disklayout_list[]=
 {
@@ -25,6 +26,7 @@ datatype * disklayout_list[]=
 	data_DiskLayout_Ensoniq_DD_800KB_xml,
 	data_DiskLayout_Ensoniq_HD_1600KB_xml,
 	data_DiskLayout_Roland_W30_S330_W50_S50_S550_DD_720KB_xml,
+	data_DiskLayout_LinnForat9K_720KB_xml,
 	data_DiskLayout_DOS_ED_2_88MB_xml,
 	data_DiskLayout_DOS_EXDD_2_5MB_xml,
 	data_DiskLayout_DOS_EXHD_4_5MB_xml,
