d78150cc52ea4c1e8c4be0d744fae4c61df9724d
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2013-07-08 19:08:54 +0000

    Copyright date update.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_cmdline/trunk@854 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/hxcfe.c
+++ sources/hxcfe.c
@@ -1,6 +1,6 @@
 /*
 //
-// Copyright (C) 2006-2012 Jean-François DEL NERO
+// Copyright (C) 2006-2013 Jean-François DEL NERO
 //
 // This file is part of HxCFloppyEmulator.
 //
@@ -909,7 +909,7 @@ int main(int argc, char* argv[])
 	hxcfe_setOutputFunc(hxcfe,&CUI_affiche);
 
 	printf("HxC Floppy Emulator : Floppy image file converter\n");
-	printf("Copyright (C) 2006-2012 Jean-Francois DEL NERO\n");
+	printf("Copyright (C) 2006-2013 Jean-Francois DEL NERO\n");
 	printf("This program comes with ABSOLUTELY NO WARRANTY\n");
 	printf("This is free software, and you are welcome to redistribute it\n");
 	printf("under certain conditions;\n\n");
