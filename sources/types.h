c16336979599549366ea2d3d856f713cfe0c1bde
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2013-07-10 05:42:54 +0000

    Big-endian / Little-endian macros added.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@860 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/types.h
+++ sources/types.h
@@ -25,3 +25,30 @@ typedef unsigned int		uint32_t;
 #define TRUE  0xFF
 #endif
 
+/////////////////////////////////////////////////////////////////////////
+
+#ifdef BIGENDIAN_HOST
+
+// big endian
+#define BIGENDIAN_WORD(wValue) (wValue)
+#define BIGENDIAN_DWORD(dwValue) (dwValue)
+
+// little endian
+
+#define LITTLEENDIAN_WORD(wValue) ( ((wValue<<8)&0xFF00) | ((wValue>>8)&0xFF) )
+#define LITTLEENDIAN_DWORD(dwValue) ( ((dwValue<<24)&0xFF000000) | ((dwValue<<16)&0xFF0000) | ((dwValue>>16)&0xFF00) | ((dwValue>>24)&0xFF) )
+
+#else
+
+//LITTLE ENDIAN HOST
+
+// big endian
+#define BIGENDIAN_WORD(wValue) ( ((wValue<<8)&0xFF00) | ((wValue>>8)&0xFF) )
+#define BIGENDIAN_DWORD(dwValue) ( ((dwValue<<24)&0xFF000000) | ((dwValue<<16)&0xFF0000) | ((dwValue>>16)&0xFF00) | ((dwValue>>24)&0xFF) )
+
+// little endian
+
+#define LITTLEENDIAN_WORD (wValue) (wValue)
+#define LITTLEENDIAN_DWORD(dwValue) (dwValue)
+
+#endif
