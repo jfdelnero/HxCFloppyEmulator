7d6739926b45ab8e509a9f297ea22edaa7a5da16
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2013-07-29 07:12:56 +0000

    DMK Loader & Writer : Some memory leaks corrected.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@904 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- libhxcfe/sources/version.h
+++ libhxcfe/sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,4,0,5
-#define STR_FILE_VERSION   "2,4,0,5\0"
-#define STR_FILE_VERSION2  "2.4.0.5"
-#define STR_DATE           "07/17/2013\0"
+#define FILE_VERSION        2,4,0,6
+#define STR_FILE_VERSION   "2,4,0,6\0"
+#define STR_FILE_VERSION2  "2.4.0.6"
+#define STR_DATE           "07/29/2013\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
