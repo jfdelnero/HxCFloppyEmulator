e373d247b388f32fbcd1e1a18e0f8a843e9c58e5
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2013-10-14 22:26:31 +0000

    New : Arburg raw file loader.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@961 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

---
+++ sources/loaders/arburg_raw_loader/arburg_raw_loader.c
@@ -0,0 +1,188 @@
+/*
+//
+// Copyright (C) 2006 - 2013 Jean-François DEL NERO
+//
+// This file is part of the HxCFloppyEmulator library
+//
+// HxCFloppyEmulator may be used and distributed without restriction provided
+// that this copyright statement is not removed from the file and that any
+// derivative work contains the original copyright notice and the associated
+// disclaimer.
+//
+// HxCFloppyEmulator is free software; you can redistribute it
+// and/or modify  it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// HxCFloppyEmulator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with HxCFloppyEmulator; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+*/
+///////////////////////////////////////////////////////////////////////////////////
+//-------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------//
+//-----------H----H--X----X-----CCCCC----22222----0000-----0000------11----------//
+//----------H----H----X-X-----C--------------2---0----0---0----0--1--1-----------//
+//---------HHHHHH-----X------C----------22222---0----0---0----0-----1------------//
+//--------H----H----X--X----C----------2-------0----0---0----0-----1-------------//
+//-------H----H---X-----X---CCCCC-----222222----0000-----0000----1111------------//
+//-------------------------------------------------------------------------------//
+//----------------------------------------------------- http://hxc2001.free.fr --//
+///////////////////////////////////////////////////////////////////////////////////
+// File : arburg_raw_loader.c
+// Contains: Arburg floppy image loader
+//
+// Written by:	DEL NERO Jean Francois
+//
+// Change History (most recent first):
+///////////////////////////////////////////////////////////////////////////////////
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "types.h"
+#include "libhxcfe.h"
+
+#include "floppy_loader.h"
+#include "floppy_utils.h"
+
+#include "tracks/arburg_track.h"
+#include "arburg_raw_loader.h"
+
+#include "libhxcadaptor.h"
+
+int ARBURG_RAW_libIsValidDiskFile(HXCFLOPPYEMULATOR* floppycontext,char * imgfile)
+{
+	int filesize;
+
+	floppycontext->hxc_printf(MSG_DEBUG,"ARBURG_RAW_libIsValidDiskFile");
+
+	if( hxc_checkfileext(imgfile,"arburgfd") )
+	{
+
+		filesize=hxc_getfilesize(imgfile);
+		if(filesize<0) 
+			return HXCFE_ACCESSERROR;
+
+		if(filesize==(0x9FE*2*80))
+		{
+			floppycontext->hxc_printf(MSG_DEBUG,"ARBURG_RAW_libIsValidDiskFile : Arburg raw file !");
+			return HXCFE_VALIDFILE;
+		}
+		else
+		{
+			floppycontext->hxc_printf(MSG_DEBUG,"ARBURG_RAW_libIsValidDiskFile : non Arburg raw file !");
+			return HXCFE_BADFILE;
+		}
+	}
+	else
+	{
+		floppycontext->hxc_printf(MSG_DEBUG,"ARBURG_RAW_libIsValidDiskFile : non Arburg raw file !");
+		return HXCFE_BADFILE;
+	}
+
+	return HXCFE_BADPARAMETER;
+}
+
+int ARBURG_RAW_libLoad_DiskFile(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydisk,char * imgfile,void * parameters)
+{
+	FILE * f;
+	unsigned short i;
+	CYLINDER*      currentcylinder;
+	SIDE*          currentside;
+	unsigned char  sector_data[0xE00];
+	unsigned short tracknumber,sidenumber;
+	
+	floppycontext->hxc_printf(MSG_DEBUG,"ARBURG_RAW_libLoad_DiskFile %s",imgfile);
+	
+	f=hxc_fopen(imgfile,"rb");
+	if(f==NULL) 
+	{
+		floppycontext->hxc_printf(MSG_ERROR,"Cannot open %s !",imgfile);
+		return HXCFE_ACCESSERROR;
+	}
+	
+	floppydisk->floppyNumberOfTrack=80;
+	floppydisk->floppyNumberOfSide=2;
+	floppydisk->floppyBitRate=250000;
+	floppydisk->floppySectorPerTrack=1;
+	floppydisk->floppyiftype=GENERIC_SHUGART_DD_FLOPPYMODE;
+
+	floppycontext->hxc_printf(MSG_DEBUG,"Arburg File : %d track, %d side, %d bit/s, %d sectors, mode %d",
+		floppydisk->floppyNumberOfTrack,
+		floppydisk->floppyNumberOfSide,
+		floppydisk->floppyBitRate,
+		floppydisk->floppySectorPerTrack,
+		floppydisk->floppyiftype);
+
+
+	floppydisk->tracks=(CYLINDER**)malloc(sizeof(CYLINDER*)*floppydisk->floppyNumberOfTrack);
+	memset(floppydisk->tracks,0,sizeof(CYLINDER*)*floppydisk->floppyNumberOfTrack);
+
+	for(i=0;i<floppydisk->floppyNumberOfTrack*floppydisk->floppyNumberOfSide;i++)
+	{			
+
+		tracknumber=i>>1;
+		sidenumber=i&1;
+	
+		fseek(f,i*0x9FE,SEEK_SET);
+		fread(&sector_data,0x9FE,1,f);
+
+		if(!floppydisk->tracks[tracknumber])
+		{
+			floppydisk->tracks[tracknumber]=allocCylinderEntry(300,floppydisk->floppyNumberOfSide);
+			currentcylinder=floppydisk->tracks[tracknumber];
+		}
+			
+
+		floppycontext->hxc_printf(MSG_DEBUG,"read track %d side %d at offset 0x%x (0x%x bytes)",
+			tracknumber,
+			sidenumber,
+			(0x9FE*tracknumber*2)+(sidenumber*0x9FE),
+			0x9FE);
+
+		currentcylinder->sides[sidenumber]=tg_alloctrack(floppydisk->floppyBitRate,ARBURG_ENCODING,currentcylinder->floppyRPM,((floppydisk->floppyBitRate/5)*2),2000,-2000,0x00);
+		currentside=currentcylinder->sides[sidenumber];					
+		currentside->number_of_sector=floppydisk->floppySectorPerTrack;
+
+		BuildArburgTrack(floppycontext,tracknumber,sidenumber,sector_data,currentside->databuffer,&currentside->tracklen,2);
+			
+	}			
+	
+	hxc_fclose(f);
+	return HXCFE_NOERROR;
+}
+
+int ARBURG_RAW_libGetPluginInfo(HXCFLOPPYEMULATOR* floppycontext,unsigned long infotype,void * returnvalue)
+{
+
+	static const char plug_id[]="ARBURG_SD";
+	static const char plug_desc[]="ARBURG dsk Loader";
+	static const char plug_ext[]="arburgfd";
+
+	plugins_ptr plug_funcs=
+	{
+		(ISVALIDDISKFILE)	ARBURG_RAW_libIsValidDiskFile,
+		(LOADDISKFILE)		ARBURG_RAW_libLoad_DiskFile,
+		(WRITEDISKFILE)		0,
+		(GETPLUGININFOS)	ARBURG_RAW_libGetPluginInfo
+	};
+
+	return libGetPluginInfo(
+			floppycontext,
+			infotype,
+			returnvalue,
+			plug_id,
+			plug_desc,
+			&plug_funcs,
+			plug_ext
+			);
+}
+
