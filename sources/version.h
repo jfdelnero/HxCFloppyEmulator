ace79268ed11c64d3f4ebd0cf962e0192973091e
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2014-01-01 09:29:10 +0000

    Copyright date updated.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1063 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/version.h
+++ sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,4,2,2
-#define STR_FILE_VERSION   "2,4,2,2\0"
-#define STR_FILE_VERSION2  "2.4.2.2"
-#define STR_DATE           "12/08/2013\0"
+#define FILE_VERSION        2,4,4,1
+#define STR_FILE_VERSION   "2,4,4,1\0"
+#define STR_FILE_VERSION2  "2.4.4.1"
+#define STR_DATE           "01/02/2014\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
