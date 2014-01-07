b0a6901c50052b8f682e11a37cb7e3c77e666b94
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2014-01-07 06:20:05 +0000

    Track viewer : first disk viewer version (WIP).
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1100 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/version.h
+++ sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,4,4,2
-#define STR_FILE_VERSION   "2,4,4,2\0"
-#define STR_FILE_VERSION2  "2.4.4.2"
-#define STR_DATE           "01/05/2014\0"
+#define FILE_VERSION        2,4,4,3
+#define STR_FILE_VERSION   "2,4,4,3\0"
+#define STR_FILE_VERSION2  "2.4.4.3"
+#define STR_DATE           "01/07/2014\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
