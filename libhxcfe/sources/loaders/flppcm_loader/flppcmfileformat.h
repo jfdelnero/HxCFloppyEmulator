74454e85b19b90ddb68cdfbcc33e50421f73496f
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2014-06-27 19:08:16 +0000

    New loader : FLP PC Magazine loader.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1143 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

---
+++ libhxcfe/sources/loaders/flppcm_loader/flppcmfileformat.h
@@ -0,0 +1,40 @@
+/*
+//
+// Copyright (C) 2006-2014 Jean-François DEL NERO
+//
+// This file is part of the HxCFloppyEmulator library
+//
+// HxCFloppyEmulator may be used and distributed without restriction provided
+// that this copyright statement is not removed from the file and that any
+// derivative work contains the original copyright notice and the associated
+// disclaimer.
+//
+// HxCFloppyEmulator is free software; you can redistribute it
+// and/or modify  it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// HxCFloppyEmulator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with HxCFloppyEmulator; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+*/
+
+#pragma pack(1)
+
+typedef struct flp_header_t_
+{
+	unsigned char  hsign[3];
+	unsigned short ver;
+	unsigned short nbsides;
+	unsigned short nbtracks;
+	unsigned short nbsectors;
+	unsigned short sectorsize;
+}flp_header_t;
+
+#pragma pack()
