30a1434b9c9bb54533c82028ff57e9b71b65df02
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2014-12-20 12:44:27 +0000

    ST file write support added.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1270 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

---
+++ libhxcfe/sources/loaders/st_loader/st_writer.c
@@ -0,0 +1,95 @@
+/*
+//
+// Copyright (C) 2006-2014 Jean-François DEL NERO
+//
+// This file is part of the HxCFloppyEmulator library
+//
+// HxCFloppyEmulator may be used and distributed without restriction provided
+// that this copyright statement is not removed from the file and that any
+// derivative work contains the original copyright notice and the associated
+// disclaimer.
+//
+// HxCFloppyEmulator is free software; you can redistribute it
+// and/or modify  it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// HxCFloppyEmulator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with HxCFloppyEmulator; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+*/
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "types.h"
+
+#include "internal_libhxcfe.h"
+#include "tracks/track_generator.h"
+#include "libhxcfe.h"
+#include "st_loader.h"
+#include "st_writer.h"
+#include "tracks/sector_extractor.h"
+#include "libhxcadaptor.h"
+
+// Main writer function
+int ST_libWrite_DiskFile(HXCFE_IMGLDR* imgldr_ctx,HXCFE_FLOPPY * floppy,char * filename)
+{
+	int nbsector;
+	int nbtrack;
+	int nbside;
+	int sectorsize;
+
+	int writeret;
+
+	FILE * stfile;
+
+	unsigned int sectorcnt_s0;
+	unsigned int sectorcnt_s1;
+
+	sectorsize = 512;
+
+	hxcfe_imgCallProgressCallback(imgldr_ctx,0,floppy->floppyNumberOfTrack*2 );
+
+	imgldr_ctx->hxcfe->hxc_printf(MSG_INFO_1,"Write ST file %s...",filename);
+
+	sectorcnt_s0 = count_sector(imgldr_ctx->hxcfe,floppy,1,0,0,sectorsize,ISOIBM_MFM_ENCODING);
+	sectorcnt_s1 = count_sector(imgldr_ctx->hxcfe,floppy,1,0,1,sectorsize,ISOIBM_MFM_ENCODING);
+
+	if(sectorcnt_s0>21 || sectorcnt_s0<9)
+	{
+		imgldr_ctx->hxcfe->hxc_printf(MSG_INFO_1,"Error : Disk format doesn't match...",filename);
+		return HXCFE_FILECORRUPTED;
+	}
+
+	nbtrack = 85;
+	while(nbtrack && !count_sector(imgldr_ctx->hxcfe,floppy,1,nbtrack-1,0,sectorsize,ISOIBM_MFM_ENCODING))
+	{
+		nbtrack--;
+	}
+
+	nbside = 1;
+	if(sectorcnt_s1)
+		nbside = 2;
+
+	nbsector = sectorcnt_s0;
+
+	imgldr_ctx->hxcfe->hxc_printf(MSG_INFO_1,"%d sectors (%d bytes), %d tracks, %d sides...",nbsector,sectorsize,nbtrack,nbside);
+
+	stfile = hxc_fopen(filename,"wb");
+	if(stfile)
+	{
+		writeret = write_raw_file(imgldr_ctx,stfile,floppy,1,nbsector,nbtrack,nbside,sectorsize,ISOIBM_MFM_ENCODING,0);
+
+		hxc_fclose(stfile);
+	}
+
+	return writeret;
+}
