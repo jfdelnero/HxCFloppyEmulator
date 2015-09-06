170e0fe57a6fddb71f2c4e821e0bbf747d6478c6
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2015-09-06 11:59:07 +0000

    Solving some more compiler issue...
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcadaptor/trunk@1332 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- libhxcadaptor/sources/fs.c
+++ libhxcadaptor/sources/fs.c
@@ -170,7 +170,7 @@ FILE *hxc_fopen (const char *filename, const char *mode)
 				case 'b':
 					oflags |= O_BINARY;
 					break;
-	#endif
+		#endif
 				case 'a':
 					rwflags = O_WRONLY;
 					oflags |= O_CREAT;
@@ -200,20 +200,44 @@ FILE *hxc_fopen (const char *filename, const char *mode)
 
 		if(append)
 		{
+#if defined (WIN32)
 			if(_lseek(fd,0,SEEK_END) == -1)
 			{
 				_close (fd);
+
 				return NULL;
 			}
+#else
+			if(lseek(fd,0,SEEK_END) == -1)
+			{
+				close (fd);
+
+				return NULL;
+			}
+#endif
 		}
 		else
 		{
+#if defined (WIN32)
 			_lseek(fd,0,SEEK_SET);
+#else
+			lseek(fd,0,SEEK_SET);
+#endif
 		}
 
+#if defined (WIN32)
 		stream = _fdopen(fd,mode);
 		if(stream == NULL)
-			_close(fd);
+		{
+			_close (fd);
+		}
+#else
+		stream = fdopen(fd,mode);
+		if(stream == NULL)
+		{
+			close (fd);
+		}
+#endif
 	}
 
 	return stream;
