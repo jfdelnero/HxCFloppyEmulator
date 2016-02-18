eb058df29a20d25bf8bc41ac6b311f18bd2c2bab
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2016-02-18 22:42:15 +0000

    DosDiskBrowser : 1.743MB & 1.764MB added.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@1386 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- HxCFloppyEmulator_software/sources/version.h
+++ HxCFloppyEmulator_software/sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,1,4,0
-#define STR_FILE_VERSION   "2,1,4,0\0"
-#define STR_FILE_VERSION2  "2.1.4.0"
-#define STR_DATE           "01/01/2016\0"
+#define FILE_VERSION        2,1,4,1
+#define STR_FILE_VERSION   "2,1,4,1\0"
+#define STR_FILE_VERSION2  "2.1.4.1"
+#define STR_DATE           "02/18/2016\0"
 #define NOMFENETRE    "HxCFloppyEmulator v"STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
