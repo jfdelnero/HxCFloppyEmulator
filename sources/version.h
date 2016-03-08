d0593bf496dca4ffb05bac5f3a590c1141415d38
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2016-03-08 20:24:00 +0000

    ImageDisk (*.IMD) export : Deleted data mark and data CRC error flags supported.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@1387 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/version.h
+++ sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,1,4,1
-#define STR_FILE_VERSION   "2,1,4,1\0"
-#define STR_FILE_VERSION2  "2.1.4.1"
-#define STR_DATE           "02/18/2016\0"
+#define FILE_VERSION        2,1,4,2
+#define STR_FILE_VERSION   "2,1,4,2\0"
+#define STR_FILE_VERSION2  "2.1.4.2"
+#define STR_DATE           "03/08/2016\0"
 #define NOMFENETRE    "HxCFloppyEmulator v"STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
