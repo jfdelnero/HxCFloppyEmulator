9b4e1eb519f8b88514a48a88cc242b7c00c7b30f
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2016-06-09 06:40:58 +0000

    Korg DSS1 xml file.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1390 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/xml_disk/DiskLayouts/LayoutsIndex.h
+++ sources/xml_disk/DiskLayouts/LayoutsIndex.h
@@ -13,6 +13,7 @@
 #include "data_DiskLayout_DOS_EXHD_6_78MB_xml.h"
 #include "data_DiskLayout_Roland_W30_S330_W50_S50_S550_DD_720KB_xml.h"
 #include "data_DiskLayout_LinnForat9K_720KB_xml.h"
+#include "data_DiskLayout_KorgDSS1_DD_800KB_xml.h"
 #include "data_DiskLayout_KorgT3_HD_1_6MB_xml.h"
 #include "data_DiskLayout_AtariST_DD_720KB_xml.h"
 #include "data_DiskLayout_OS9_1280KB_xml.h"
@@ -45,6 +46,7 @@ datatype * disklayout_list[]=
 	data_DiskLayout_Emax_DD_800KB_xml,
 	data_DiskLayout_Roland_W30_S330_W50_S50_S550_DD_720KB_xml,
 	data_DiskLayout_LinnForat9K_720KB_xml,
+	data_DiskLayout_KorgDSS1_DD_800KB_xml,
 	data_DiskLayout_KorgT3_HD_1_6MB_xml,
 	data_DiskLayout_GeneralMusic_GEM_S3_1600KB_xml,
 	data_DiskLayout_GeneralMusic_GEM_WX_Expander_xml,
