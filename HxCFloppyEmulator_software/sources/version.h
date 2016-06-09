31773c986a661f31ad99050735971a603bcf4327
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2016-06-09 06:40:58 +0000

    Korg DSS1 xml file.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@1390 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- HxCFloppyEmulator_software/sources/version.h
+++ HxCFloppyEmulator_software/sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,1,4,3
-#define STR_FILE_VERSION   "2,1,4,3\0"
-#define STR_FILE_VERSION2  "2.1.4.3"
-#define STR_DATE           "03/30/2016\0"
+#define FILE_VERSION        2,1,4,5
+#define STR_FILE_VERSION   "2,1,4,5\0"
+#define STR_FILE_VERSION2  "2.1.4.5"
+#define STR_DATE           "09/06/2016\0"
 #define NOMFENETRE    "HxCFloppyEmulator v"STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
