18276dd5bf2383339f9da72ceb52b8904af77f44
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2016-12-30 16:37:15 +0000

    Makefiles update : debug mode support + -lXext -lXfixes added for the gui.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1403 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/version.h
+++ sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,8,9,10
-#define STR_FILE_VERSION   "2,8,9,10\0"
-#define STR_FILE_VERSION2  "2.8.9.10"
-#define STR_DATE           "09/06/2016\0"
+#define FILE_VERSION        2,8,9,11
+#define STR_FILE_VERSION   "2,8,9,11\0"
+#define STR_FILE_VERSION2  "2.8.9.11"
+#define STR_DATE           "20/06/2016\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
