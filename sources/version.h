8720861c9029bc838a56080765b4a8b3c4fb0f7d
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-01-21 14:14:26 +0000

    Space needed with C++11
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@1488 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/version.h
+++ sources/version.h
@@ -2,7 +2,7 @@
 #define STR_FILE_VERSION   "2,1,7,0\0"
 #define STR_FILE_VERSION2  "2.1.7.0"
 #define STR_DATE           "27 July 2017\0"
-#define NOMFENETRE    "HxCFloppyEmulator v"STR_FILE_VERSION2
+#define NOMFENETRE    "HxCFloppyEmulator v" STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
 #define PRODUCTVER     FILE_VERSION
