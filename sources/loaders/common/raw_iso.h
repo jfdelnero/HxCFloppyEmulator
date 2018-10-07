efaab96ba2a5bf698f0acb540f320be9be4b50d6
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-10-07 20:48:56 +0000

    raw_iso_loader generic raw file loader.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1630 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

---
+++ sources/loaders/common/raw_iso.h
@@ -0,0 +1,22 @@
+
+typedef struct raw_iso_cfg_
+{
+	int32_t number_of_tracks;
+	int32_t number_of_sides;
+	int32_t number_of_sectors_per_track;
+	int32_t sector_size;
+	int32_t start_sector_id;
+	int32_t gap3;
+	int32_t interleave;
+	int32_t skew_per_track;
+	int32_t skew_per_side;
+	int32_t bitrate;
+	int32_t rpm;
+	int32_t track_format;
+	int32_t interface_mode;
+	uint8_t fill_value;
+}raw_iso_cfg;
+
+int raw_iso_loader(HXCFE_IMGLDR * imgldr_ctx,HXCFE_FLOPPY * floppydisk, FILE * f_img , unsigned char * imagebuffer, int size, raw_iso_cfg * cfg);
+
+void raw_iso_setdefcfg(raw_iso_cfg *rawcfg);
