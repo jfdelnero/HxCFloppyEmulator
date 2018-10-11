1efe3a691d991203627bec764a2b28bb5afb57ee
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-10-11 22:11:33 +0000

    switch to the generic raw loader.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1653 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- libhxcfe/sources/loaders/sad_loader/sad_loader.c
+++ libhxcfe/sources/loaders/sad_loader/sad_loader.c
@@ -50,16 +50,15 @@
 #include "types.h"
 
 #include "internal_libhxcfe.h"
-#include "tracks/track_generator.h"
 #include "libhxcfe.h"
-
+#include "libhxcadaptor.h"
 #include "floppy_loader.h"
-#include "floppy_utils.h"
+#include "tracks/track_generator.h"
+
+#include "loaders/common/raw_iso.h"
 
 #include "sad_loader.h"
 #include "sad_fileformat.h"
-
-#include "libhxcadaptor.h"
 
 int SAD_libIsValidDiskFile(HXCFE_IMGLDR * imgldr_ctx,char * imgfile)
 {
@@ -103,82 +102,50 @@ int SAD_libIsValidDiskFile(HXCFE_IMGLDR * imgldr_ctx,char * imgfile)
 	return HXCFE_BADPARAMETER;
 }
 
-
-
 int SAD_libLoad_DiskFile(HXCFE_IMGLDR * imgldr_ctx,HXCFE_FLOPPY * floppydisk,char * imgfile,void * parameters)
 {
-	FILE * f;
-	int i,j;
-	unsigned int file_offset;
-	unsigned char* trackdata;
-	int gap3len,interleave;
-	int rpm;
-	int sectorsize;
-	int trackformat,skew;
-	HXCFE_CYLINDER* currentcylinder;
+	raw_iso_cfg rawcfg;
+	FILE * f_img;
+	int ret;
 	SAD_HEADER sadh;
 
 	imgldr_ctx->hxcfe->hxc_printf(MSG_DEBUG,"SAD_libLoad_DiskFile %s",imgfile);
 
-	f = hxc_fopen(imgfile,"rb");
-	if( f == NULL )
+	f_img = hxc_fopen(imgfile,"rb");
+	if( f_img == NULL )
 	{
 		imgldr_ctx->hxcfe->hxc_printf(MSG_ERROR,"SAD_libLoad_DiskFile : Cannot open %s !",imgfile);
 		return HXCFE_ACCESSERROR;
 	}
 
 	memset(&sadh,0,sizeof(SAD_HEADER));
-	hxc_fread(&sadh,sizeof(SAD_HEADER),f);
+	hxc_fread(&sadh,sizeof(SAD_HEADER),f_img);
 	if(strncmp((char*)sadh.abSignature,SAD_SIGNATURE,sizeof SAD_SIGNATURE - 1))
 	{
-		hxc_fclose(f);
+		hxc_fclose(f_img);
 		imgldr_ctx->hxcfe->hxc_printf(MSG_DEBUG,"SAD_libLoad_DiskFile : non SAD file !");
 		return HXCFE_BADFILE;
 	}
 
-	floppydisk->floppyNumberOfTrack=sadh.bTracks;
-	floppydisk->floppyNumberOfSide=sadh.bSides;
-	floppydisk->floppySectorPerTrack=sadh.bSectors;
-	sectorsize=sadh.bSectorSizeDiv64*64;
-	gap3len=35;
-	interleave=1;
-	skew=0;
-	trackformat=ISOFORMAT_DD;
+	raw_iso_setdefcfg(&rawcfg);
 
-	floppydisk->floppyBitRate=250000;
-	floppydisk->floppyiftype=GENERIC_SHUGART_DD_FLOPPYMODE;
-	floppydisk->tracks=(HXCFE_CYLINDER**)malloc(sizeof(HXCFE_CYLINDER*)*floppydisk->floppyNumberOfTrack);
+	rawcfg.number_of_tracks = sadh.bTracks;
+	rawcfg.number_of_sides = sadh.bSides;
+	rawcfg.number_of_sectors_per_track = sadh.bSectors;
+	rawcfg.sector_size = sadh.bSectorSizeDiv64*64;
+	rawcfg.gap3 = 35;
+	rawcfg.interleave = 1;
+	rawcfg.track_format = ISOFORMAT_DD;
+	rawcfg.bitrate = 250000;
+	rawcfg.interface_mode = GENERIC_SHUGART_DD_FLOPPYMODE;
+	rawcfg.rpm = 300; // normal rpm
+	rawcfg.trk_grouped_by_sides = 1;
 
-	rpm=300; // normal rpm
+	ret = raw_iso_loader(imgldr_ctx, floppydisk, f_img, 0, 0, &rawcfg);
 
-	imgldr_ctx->hxcfe->hxc_printf(MSG_INFO_1,"SAD_libLoad_DiskFile : %d tracks, %d side(s), %d sectors/track, gap3:%d, interleave:%d,rpm:%d",floppydisk->floppyNumberOfTrack,floppydisk->floppyNumberOfSide,floppydisk->floppySectorPerTrack,gap3len,interleave,rpm);
+	hxc_fclose(f_img);
 
-	trackdata=(unsigned char*)malloc(sectorsize*floppydisk->floppySectorPerTrack);
-
-	for(j=0;j<floppydisk->floppyNumberOfTrack;j++)
-	{
-		floppydisk->tracks[j]=allocCylinderEntry(rpm,floppydisk->floppyNumberOfSide);
-		currentcylinder=floppydisk->tracks[j];
-
-		for(i=0;i<floppydisk->floppyNumberOfSide;i++)
-		{
-			hxcfe_imgCallProgressCallback(imgldr_ctx,(j<<1) + (i&1),floppydisk->floppyNumberOfTrack*2 );
-
-			file_offset=(sectorsize*(j*floppydisk->floppySectorPerTrack))+
-				(sectorsize*(floppydisk->floppyNumberOfTrack*floppydisk->floppySectorPerTrack)*i) + sizeof(SAD_HEADER) ;
-			fseek (f , file_offset , SEEK_SET);
-			hxc_fread(trackdata,sectorsize*floppydisk->floppySectorPerTrack,f);
-
-			currentcylinder->sides[i]=tg_generateTrack(trackdata,sectorsize,floppydisk->floppySectorPerTrack,(unsigned char)j,(unsigned char)i,1,interleave,((j<<1)|(i&1))*skew,floppydisk->floppyBitRate,currentcylinder->floppyRPM,trackformat,gap3len,0,2500|NO_SECTOR_UNDER_INDEX,-2500);
-		}
-	}
-
-	free(trackdata);
-
-	imgldr_ctx->hxcfe->hxc_printf(MSG_INFO_1,"SAD_libLoad_DiskFile : track file successfully loaded and encoded!");
-
-	hxc_fclose(f);
-	return HXCFE_NOERROR;
+	return ret;
 }
 
 int SAD_libGetPluginInfo(HXCFE_IMGLDR * imgldr_ctx,uint32_t infotype,void * returnvalue)
