e4177db0172983ccb809858e268d748dd28bca56
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-02-16 12:39:11 +0000

    Coat-A-Matic 9400 / Robox RC9400 raw file support.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1708 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- libhxcfe/sources/version.h
+++ libhxcfe/sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,9,2,5
-#define STR_FILE_VERSION   "2,9,2,5\0"
-#define STR_FILE_VERSION2  "2.9.2.5"
-#define STR_DATE           "28 January 2019\0"
+#define FILE_VERSION        2,9,2,6
+#define STR_FILE_VERSION   "2,9,2,6\0"
+#define STR_FILE_VERSION2  "2.9.2.6"
+#define STR_DATE           "16 February 2019\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
