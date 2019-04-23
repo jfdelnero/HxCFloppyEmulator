039b1abc3f7c56d53adaa01c7a355967a0a412a1
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-04-23 07:03:49 +0000

    Fix Acorn ADF support regression.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1716 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- libhxcfe/sources/version.h
+++ libhxcfe/sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,9,2,7
-#define STR_FILE_VERSION   "2,9,2,7\0"
-#define STR_FILE_VERSION2  "2.9.2.7"
-#define STR_DATE           "29 March 2019\0"
+#define FILE_VERSION        2,9,2,8
+#define STR_FILE_VERSION   "2,9,2,8\0"
+#define STR_FILE_VERSION2  "2.9.2.8"
+#define STR_DATE           "23 April 2019\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
