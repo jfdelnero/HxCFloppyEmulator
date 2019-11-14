eaf0d0ef8f8cb630b3edb36143eaa0735069304f
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-11-14 23:33:31 +0000

    wip : script support.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@1785 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

---
+++ sources/misc/script_exec.c
@@ -0,0 +1,340 @@
+/*
+//
+// Copyright (C) 2006-2019 Jean-François DEL NERO
+//
+// This file is part of the HxCFloppyEmulator library
+//
+// HxCFloppyEmulator may be used and distributed without restriction provided
+// that this copyright statement is not removed from the file and that any
+// derivative work contains the original copyright notice and the associated
+// disclaimer.
+//
+// HxCFloppyEmulator is free software; you can redistribute it
+// and/or modify  it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// HxCFloppyEmulator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with HxCFloppyEmulator; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+*/
+///////////////////////////////////////////////////////////////////////////////////
+//-------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------//
+//-----------H----H--X----X-----CCCCC----22222----0000-----0000------11----------//
+//----------H----H----X-X-----C--------------2---0----0---0----0--1--1-----------//
+//---------HHHHHH-----X------C----------22222---0----0---0----0-----1------------//
+//--------H----H----X--X----C----------2-------0----0---0----0-----1-------------//
+//-------H----H---X-----X---CCCCC-----222222----0000-----0000----1111------------//
+//-------------------------------------------------------------------------------//
+//----------------------------------------------------- http://hxc2001.free.fr --//
+///////////////////////////////////////////////////////////////////////////////////
+// File : script_exec.c
+// Contains: script execution functions
+//
+// Written by: Jean-François DEL NERO
+//
+// Change History (most recent first):
+///////////////////////////////////////////////////////////////////////////////////
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "types.h"
+
+#include "internal_libhxcfe.h"
+#include "tracks/track_generator.h"
+#include "sector_search.h"
+#include "libhxcfe.h"
+#include "floppy_loader.h"
+
+#include "libhxcadaptor.h"
+
+#include "version.h"
+
+#define MAX_CFG_STRING_SIZE 1024
+
+typedef int (* KW_FUNC)(HXCFE * context, char * line, int cmd);
+
+enum
+{
+	VERSION_CMD = 0,
+	SET_ENV_CMD
+};
+
+typedef struct kw_list_
+{
+	char * keyword;
+	KW_FUNC func;
+	int cmd;
+}kw_list;
+
+static int is_end_line(char c)
+{
+	if( c == 0 || c == '#' || c == '\r' || c == '\n' )
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+static int is_space(char c)
+{
+	if( c == ' ' || c == '\t' )
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+static int get_next_word(char * line, int offset)
+{
+	while( !is_end_line(line[offset]) && ( line[offset] == ' ' ) )
+	{
+		offset++;
+	}
+
+	return offset;
+}
+
+static int copy_param(char * dest, char * line, int offs)
+{
+	int i,insidequote;
+
+	i = 0;
+	insidequote = 0;
+	while( !is_end_line(line[offs]) && ( insidequote || !is_space(line[offs]) ) )
+	{
+		if(line[offs] != '"')
+		{
+			if(dest)
+				dest[i] = line[offs];
+
+			i++;
+		}
+		else
+		{
+			if(insidequote)
+				insidequote = 0;
+			else
+				insidequote = 1;
+		}
+
+		offs++;
+	}
+
+	if(dest)
+		dest[i] = 0;
+
+	return offs;
+}
+
+static int get_param_offset(char * line, int param)
+{
+	int param_cnt, offs;
+
+	offs = 0;
+	offs = get_next_word(line, offs);
+
+	param_cnt = 0;
+	do
+	{
+		offs = copy_param(NULL, line, offs);
+
+		offs = get_next_word( line, offs );
+
+		if(line[offs] == 0 || line[offs] == '#')
+			return -1;
+
+		param_cnt++;
+	}while( param_cnt < param );
+
+	return offs;
+}
+
+static int get_param(char * line, int param_offset,char * param)
+{
+	int offs;
+
+	offs = get_param_offset(line, param_offset);
+
+	if(offs>=0)
+	{
+		offs = copy_param(param, line, offs);
+
+		return 1;
+	}
+
+	return -1;
+}
+
+static int extract_cmd(char * line, char * command)
+{
+	int offs,i;
+
+	i = 0;
+	offs = 0;
+
+	offs = get_next_word(line, offs);
+
+	if( !is_end_line(line[offs]) )
+	{
+		while( !is_end_line(line[offs]) && !is_space(line[offs]) )
+		{
+			command[i] = line[offs];
+			offs++;
+			i++;
+		}
+
+		command[i] = 0;
+
+		return i;
+	}
+
+	return 0;
+}
+
+static int get_hex_param(HXCFE * context, char * line,int cmd)
+{
+	int i;
+	char tmp_txt[MAX_CFG_STRING_SIZE];
+	unsigned long param_value;
+
+	i = get_param(line, 1,tmp_txt);
+
+	if( i >= 0 )
+	{
+		param_value = strtol(tmp_txt,0,16);
+		switch(cmd)
+		{
+
+		}
+	}
+
+	return 0;
+}
+
+static int print_version(HXCFE * context, char * line,int cmd)
+{
+	context->hxc_printf(MSG_INFO_1,"v"STR_FILE_VERSION2);
+
+	return 0;
+}
+
+static int get_str_param(HXCFE * context, char * line,int cmd)
+{
+	int i;
+	char tmp_txt[MAX_CFG_STRING_SIZE];
+
+	i = get_param(line, 1,tmp_txt);
+
+	if( i >= 0 )
+	{
+		switch(cmd)
+		{
+		}
+	}
+
+	return 0;
+}
+
+static int set_env_var(HXCFE * context, char * line,int cmd)
+{
+
+}
+
+kw_list kwlist[] =
+{
+	{"version",             print_version,      VERSION_CMD},
+	{"set",                 set_env_var,        SET_ENV_CMD},
+
+	//{"exec",          get_hex_param,      EXEC_CMD},
+
+	{ 0, 0, 0 }
+};
+
+static int exec_cmd(HXCFE * context, char * command,char * line)
+{
+	int i;
+
+	i = 0;
+	while(kwlist[i].func)
+	{
+		if( !strcmp(kwlist[i].keyword,command) )
+		{
+			kwlist[i].func(context, line, kwlist[i].cmd);
+			return 1;
+		}
+
+		i++;
+	}
+
+	return 0;
+}
+
+static int execute_line(HXCFE * context,char * line)
+{
+	char command[MAX_CFG_STRING_SIZE];
+
+	command[0] = 0;
+
+	if( extract_cmd(line, command) )
+	{
+		if(strlen(command))
+		{
+			if( !exec_cmd(context, command,line))
+			{
+				context->hxc_printf(MSG_ERROR,"Line syntax error : %s",line);
+
+				return 0;
+			}
+		}
+
+		return 1;
+	}
+
+	return 0;
+}
+
+int hxcfe_exec_script_file(HXCFE* context, char * script_path)
+{
+	int err = 0;
+	FILE * f;
+	char line[MAX_CFG_STRING_SIZE];
+
+	f = fopen(script_path,"r");
+	if(f)
+	{
+		do
+		{
+			if(!fgets(line,sizeof(line),f))
+				break;
+
+			if(feof(f))
+				break;
+
+			execute_line(context, line);
+		}while(1);
+
+		fclose(f);
+	}
+	else
+	{
+		context->hxc_printf(MSG_ERROR,"Can't open %s ! Using default settings...",script_path);
+	}
+
+	return err;
+}
