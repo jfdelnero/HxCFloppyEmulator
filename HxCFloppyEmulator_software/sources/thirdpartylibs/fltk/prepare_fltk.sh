7c8f50460a188bf59b6035ee24c650174c2fb078
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-12-08 10:09:37 +0000

    do the job only if fltk-1.3.x doesn't exist (removed by mrproper).
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@1876 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- HxCFloppyEmulator_software/sources/thirdpartylibs/fltk/prepare_fltk.sh
+++ HxCFloppyEmulator_software/sources/thirdpartylibs/fltk/prepare_fltk.sh
@@ -1,5 +1,7 @@
-wget https://www.fltk.org/pub/fltk/1.3.5/fltk-1.3.5-source.tar.gz -nc || exit 1
-rm -rf fltk-1.3.x/*
-tar -xzf fltk-1.3.5-source.tar.gz
-mv fltk-1.3.5 fltk-1.3.x
-patch -i ../fltk-1.3.5-vc6.patch -p1 -d fltk-1.3.x
+if [ ! -d fltk-1.3.x ]; then
+	wget https://www.fltk.org/pub/fltk/1.3.5/fltk-1.3.5-source.tar.gz -nc || exit 1
+	tar -xzf fltk-1.3.5-source.tar.gz
+	mv fltk-1.3.5 fltk-1.3.x
+	patch -i ../fltk-1.3.5-vc6.patch -p1 -d fltk-1.3.x
+fi
+
