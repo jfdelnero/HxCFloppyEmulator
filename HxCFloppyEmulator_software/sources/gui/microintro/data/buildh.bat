c333e29bb76635480915fa3c406ad1f6b07856b2
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2020-04-19 06:35:08 +0000

    hxc2001 & pauline background logos.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@2001 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- HxCFloppyEmulator_software/sources/gui/microintro/data/buildh.bat
+++ HxCFloppyEmulator_software/sources/gui/microintro/data/buildh.bat
@@ -1,6 +1,8 @@
 ..\converttools\bmptoh sob.bmp -BMP8P
 ..\converttools\bmptoh hxc2001.bmp -BMP8P
+..\converttools\bmptoh hxc2001_2.bmp -BMP8P
 ..\converttools\bmptoh hxc2001_backgnd.bmp -BMP8P
+..\converttools\bmptoh pauline.bmp -BMP8P
 ..\converttools\bmptoh zandax_supplydas_booze.mod   -DATA
 ..\converttools\bmptoh maktone_class_cracktro15.mod -DATA
 ..\converttools\bmptoh jozz_cognition.mod -DATA
