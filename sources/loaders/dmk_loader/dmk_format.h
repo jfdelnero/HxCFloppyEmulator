5360a32d69ab176506b209a733d93211eff58304
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2020-08-12 12:54:48 +0000

    Fix single density/FM IDAM random bad clock issue.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@2167 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/loaders/dmk_loader/dmk_format.h
+++ sources/loaders/dmk_loader/dmk_format.h
@@ -10,7 +10,10 @@ typedef struct dmk_header_
 	uint8_t  write_protected;
 	uint8_t  track_number;
 	uint16_t track_len;
-	uint8_t  flags;
+	uint8_t  flags; // 0x10 - Single sided
+					// 0x40 - Single density
+					// 0x80 - Ignore density
+	
 	uint8_t  rsvd_1[(0xB-0x5)+1];
 	uint8_t  rsvd_2[(0xF-0xC)+1];	
 }dmk_header;
