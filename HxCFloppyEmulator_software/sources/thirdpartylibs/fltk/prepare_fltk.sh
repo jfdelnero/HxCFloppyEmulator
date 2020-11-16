0c1e109e0ff427950f6c7d46c9997ed9076621b8
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2020-11-16 21:31:24 +0000

    fltk 1.4.x support.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@2263 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- HxCFloppyEmulator_software/sources/thirdpartylibs/fltk/prepare_fltk.sh
+++ HxCFloppyEmulator_software/sources/thirdpartylibs/fltk/prepare_fltk.sh
@@ -1,7 +1,17 @@
-if [ ! -d fltk-1.3.x ]; then
-	wget https://www.fltk.org/pub/fltk/1.3.5/fltk-1.3.5-source.tar.gz -nc || exit 1
-	tar -xzf fltk-1.3.5-source.tar.gz
-	mv fltk-1.3.5 fltk-1.3.x
-	patch -i ../fltk-1.3.5-vc6.patch -p1 -d fltk-1.3.x
+#export DOWNLOADURL=https://www.fltk.org/pub/fltk/1.3.5/fltk-1.3.5-source.tar.gz
+#export ARCHIVENAMEBASE=fltk-1.3.5-source
+#export PATCHFILE=fltk-1.3.5-vc6.patch
+#export FOLDERNAME=fltk-1.3.5
+
+export DOWNLOADURL=https://www.fltk.org/pub/fltk/snapshots/fltk-1.4.x-20201113-a4bacf83.tar.gz
+export ARCHIVENAMEBASE=fltk-1.4.x-20201113-a4bacf83
+export PATCHFILE=fltk-1.4.x-vc6.patch
+export FOLDERNAME=fltk-1.4.x-20201113-a4bacf83
+
+if [ ! -d fltk-1.x.x ]; then
+	wget $DOWNLOADURL -nc || exit 1
+	tar -xzf ${ARCHIVENAMEBASE}.tar.gz
+	mv ${FOLDERNAME} fltk-1.x.x
+	patch -i ../${PATCHFILE} -p1 -d fltk-1.x.x
 fi
 
