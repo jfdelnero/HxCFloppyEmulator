d6439483ba1ed0a172e453265dfb748dce4753c3
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2020-12-29 10:36:42 +0000

    Stay with the 1.3.5 for the moment. The 1.4.X seems to be too young yet.
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@2350 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- HxCFloppyEmulator_software/sources/thirdpartylibs/fltk/prepare_fltk.sh
+++ HxCFloppyEmulator_software/sources/thirdpartylibs/fltk/prepare_fltk.sh
@@ -1,15 +1,20 @@
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	export DOWNLOADURL=https://www.fltk.org/pub/fltk/snapshots/fltk-1.4.x-20201127-d7985607.tar.gz
-	export ARCHIVENAMEBASE=fltk-1.4.x-20201127-d7985607
-	export PATCHFILE=fltk-1.4.x-vc6.patch
-	export FOLDERNAME=fltk-1.4.x-20201127-d7985607
-else
-	export DOWNLOADURL=https://www.fltk.org/pub/fltk/1.3.5/fltk-1.3.5-source.tar.gz
-	export ARCHIVENAMEBASE=fltk-1.3.5-source
-	export PATCHFILE=fltk-1.3.5-vc6.patch
-	export FOLDERNAME=fltk-1.3.5
-fi
+#if [[ "$OSTYPE" == "darwin"* ]]; then
+#	export DOWNLOADURL=https://www.fltk.org/pub/fltk/snapshots/fltk-1.4.x-20201127-d7985607.tar.gz
+#	export ARCHIVENAMEBASE=fltk-1.4.x-20201127-d7985607
+#	export PATCHFILE=fltk-1.4.x-vc6.patch
+#	export FOLDERNAME=fltk-1.4.x-20201127-d7985607
+#else
+#	export DOWNLOADURL=https://www.fltk.org/pub/fltk/1.3.5/fltk-1.3.5-source.tar.gz
+#	export ARCHIVENAMEBASE=fltk-1.3.5-source
+#	export PATCHFILE=fltk-1.3.5-vc6.patch
+#	export FOLDERNAME=fltk-1.3.5
+#fi
+
+export DOWNLOADURL=https://www.fltk.org/pub/fltk/1.3.5/fltk-1.3.5-source.tar.gz
+export ARCHIVENAMEBASE=fltk-1.3.5-source
+export PATCHFILE=fltk-1.3.5-vc6.patch
+export FOLDERNAME=fltk-1.3.5
 
 if [ ! -d fltk-1.x.x ]; then
 	wget $DOWNLOADURL -nc || exit 1
