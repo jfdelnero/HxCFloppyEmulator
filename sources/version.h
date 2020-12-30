5f7d30f7d2a7321aedf11b733a58abbad00a81c5
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2020-12-30 22:27:03 +0000

    v2.5.4.2
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@2367 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- sources/version.h
+++ sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,5,4,1
-#define STR_FILE_VERSION   "2,5,4,1\0"
-#define STR_FILE_VERSION2  "2.5.4.1"
-#define STR_DATE           "29 December 2020\0"
+#define FILE_VERSION        2,5,4,2
+#define STR_FILE_VERSION   "2,5,4,2\0"
+#define STR_FILE_VERSION2  "2.5.4.2"
+#define STR_DATE           "30 December 2020\0"
 #define NOMFENETRE    "HxCFloppyEmulator v" STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
