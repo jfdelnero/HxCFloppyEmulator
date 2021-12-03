5325f2aa570b5601ceaea590d7c7ce61f78e05cf
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2021-12-03 23:13:07 +0000

    fltk-1.3.8
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/HxCFloppyEmulator_software/trunk@2511 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- HxCFloppyEmulator_software/sources/thirdpartylibs/fltk/prepare_fltk.sh
+++ HxCFloppyEmulator_software/sources/thirdpartylibs/fltk/prepare_fltk.sh
@@ -1,8 +1,8 @@
 
-export DOWNLOADURL=https://www.fltk.org/pub/fltk/1.3.6/fltk-1.3.6-source.tar.gz
-export ARCHIVENAMEBASE=fltk-1.3.6-source
-export PATCHFILE=fltk-1.3.6.patch
-export FOLDERNAME=fltk-1.3.6
+export DOWNLOADURL=https://www.fltk.org/pub/fltk/1.3.8/fltk-1.3.8-source.tar.gz
+export ARCHIVENAMEBASE=fltk-1.3.8-source
+export PATCHFILE=fltk-1.3.8.patch
+export FOLDERNAME=fltk-1.3.8
 
 if [ ! -d fltk-1.x.x ]; then
 	wget $DOWNLOADURL -nc || exit 1
