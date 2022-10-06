3110a8c86eb3cf0626d9fd4c35be76d6ba02b613
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2022-10-06 21:30:57 +0000

    v2.13.18.1
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/libhxcfe/trunk@2612 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- libhxcfe/sources/version.h
+++ libhxcfe/sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,13,17,2
-#define STR_FILE_VERSION   "2,13,17,2\0"
-#define STR_FILE_VERSION2  "2.13.17.2"
-#define STR_DATE           "3 Oct 2022\0"
+#define FILE_VERSION        2,13,18,1
+#define STR_FILE_VERSION   "2,13,18,1\0"
+#define STR_FILE_VERSION2  "2.13.18.1"
+#define STR_DATE           "6 Oct 2022\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
