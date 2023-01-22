3a6054a051e89ce38c5256dc11b6991d71eb1198
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2023-01-22 22:53:46 +0100

    (c) dates updated.

--- HxCFloppyEmulator_cmdline/sources/hxcfe.c
+++ HxCFloppyEmulator_cmdline/sources/hxcfe.c
@@ -1,6 +1,6 @@
 /*
 //
-// Copyright (C) 2006-2022 Jean-François DEL NERO
+// Copyright (C) 2006-2023 Jean-François DEL NERO
 //
 // This file is part of HxCFloppyEmulator.
 //
@@ -1077,7 +1077,7 @@ int main(int argc, char* argv[])
 	hxcfe_setScriptOutputFunc(hxcfe,&SCRIPT_CUI_affiche);
 
 	printf("HxC Floppy Emulator : Floppy image file converter v2.5.5.1\n");
-	printf("Copyright (C) 2006-2022 Jean-Francois DEL NERO\n");
+	printf("Copyright (C) 2006-2023 Jean-Francois DEL NERO\n");
 	printf("This program comes with ABSOLUTELY NO WARRANTY\n");
 	printf("This is free software, and you are welcome to redistribute it\n");
 	printf("under certain conditions;\n\n");
