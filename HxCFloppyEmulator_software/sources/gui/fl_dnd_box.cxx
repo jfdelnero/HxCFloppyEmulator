0a8618219c517298a0155eee416f8545938b50ad
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2023-12-16 09:40:03 +0100

    separate declaration and implementation.

--- HxCFloppyEmulator_software/sources/gui/fl_dnd_box.cxx
+++ HxCFloppyEmulator_software/sources/gui/fl_dnd_box.cxx
@@ -1,1 +1,79 @@
+#include <FL/Fl_Box.H>
+#include "fl_dnd_box.cxx"
 
+class Fl_DND_Box ::
+{
+	static void callback_deferred(void *v)
+	{
+		Fl_DND_Box *w = (Fl_DND_Box*)v;
+
+		w->do_callback();
+	}
+
+	Fl_DND_Box(int X, int Y, int W, int H, const char *L = 0) 
+		: Fl_Box(X,Y,W,H,L), evt(FL_NO_EVENT), evt_txt(0), evt_len(0);
+	{
+		labeltype(FL_NO_LABEL);
+		box(FL_NO_BOX);
+		clear_visible_focus();
+	}
+
+	virtual ~Fl_DND_Box()
+	{
+		free(evt_txt);
+		evt_txt = NULL;
+	}
+
+	int event()
+	{
+		return evt;
+	}
+
+	const char* event_text()
+	{
+		return evt_txt;
+	}
+
+	int event_length()
+	{
+		return evt_len;
+	}
+
+	int handle(int e)
+	{
+		switch(e)
+		{
+			case FL_DND_ENTER:
+			case FL_DND_RELEASE:
+			case FL_DND_LEAVE:
+			case FL_DND_DRAG:
+				evt = e;
+				return 1;
+
+			case FL_PASTE:
+				evt = e;
+
+				// make a copy of the DND payload
+				evt_len = Fl::event_length();
+
+				free(evt_txt);
+
+				evt_txt = (char*)calloc( 1, evt_len+1 );
+				if(evt_txt)
+					strncpy(evt_txt, Fl::event_text(),evt_len);
+
+				// If there is a callback registered, call it.
+				// The callback must access Fl::event_text() to
+				// get the string or file path that was dropped.
+				// Note that do_callback() is not called directly.
+				// Instead it will be executed by the FLTK main-loop
+				// once we have finished handling the DND event.
+				// This allows caller to popup a window or change widget focus.
+				if(callback() && ((when() & FL_WHEN_RELEASE) || (when() & FL_WHEN_CHANGED)))
+					Fl::add_timeout(0.0, Fl_DND_Box::callback_deferred, (void*)this);
+				return 1;
+		}
+
+		return Fl_Box::handle(e);
+	}
+};
--- HxCFloppyEmulator_software/sources/gui/fl_dnd_box.h
+++ HxCFloppyEmulator_software/sources/gui/fl_dnd_box.h
@@ -2,90 +2,23 @@
 
 class Fl_DND_Box : public Fl_Box
 {
-    public:
+	public:
 
-        static void callback_deferred(void *v)
-        {
-            Fl_DND_Box *w = (Fl_DND_Box*)v;
+		static void callback_deferred(void *v);
+		Fl_DND_Box(int X, int Y, int W, int H, const char *L = 0);
 
-            w->do_callback();
-        }
+		virtual ~Fl_DND_Box();
+		int event();
+		const char* event_text();
+		int event_length();
+		int handle(int e);
 
-        Fl_DND_Box(int X, int Y, int W, int H, const char *L = 0)
-                : Fl_Box(X,Y,W,H,L), evt(FL_NO_EVENT), evt_txt(0), evt_len(0)
-        {
-            labeltype(FL_NO_LABEL);
-            box(FL_NO_BOX);
-            clear_visible_focus();
-        }
+	protected:
 
-        virtual ~Fl_DND_Box()
-        {
-            free(evt_txt);
-            evt_txt = NULL;
-        }
-
-        int event()
-        {
-            return evt;
-        }
-
-        const char* event_text()
-        {
-            return evt_txt;
-        }
-
-        int event_length()
-        {
-            return evt_len;
-        }
-
-        int handle(int e)
-        {
-            switch(e)
-            {
-                case FL_DND_ENTER:
-                case FL_DND_RELEASE:
-                case FL_DND_LEAVE:
-                case FL_DND_DRAG:
-                    evt = e;
-                    return 1;
-
-
-                case FL_PASTE:
-                    evt = e;
-
-                    // make a copy of the DND payload
-                    evt_len = Fl::event_length();
-
-                    free(evt_txt);
-
-                    evt_txt = (char*)calloc( 1, evt_len+1 );
-                    if(evt_txt)
-                        strncpy(evt_txt, Fl::event_text(),evt_len);
-
-                    // If there is a callback registered, call it.
-                    // The callback must access Fl::event_text() to
-                    // get the string or file path that was dropped.
-                    // Note that do_callback() is not called directly.
-                    // Instead it will be executed by the FLTK main-loop
-                    // once we have finished handling the DND event.
-                    // This allows caller to popup a window or change widget focus.
-                    if(callback() && ((when() & FL_WHEN_RELEASE) || (when() & FL_WHEN_CHANGED)))
-                        Fl::add_timeout(0.0, Fl_DND_Box::callback_deferred, (void*)this);
-                    return 1;
-            }
-
-            return Fl_Box::handle(e);
-        }
-
-    protected:
-        // The event which caused Fl_DND_Box to execute its callback
-        int evt;
-
-        char *evt_txt;
-        int evt_len;
+		// The event which caused Fl_DND_Box to execute its callback
+		int evt;
+		char *evt_txt;
+		int evt_len;
 };
 
 extern void dnd_bc_cb(Fl_Widget *o, void *v);
-
