cee80cda51c4299467fd6164ec1540f0ef7df42b
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2024-12-17 00:34:25 +0100

    v2.16.8.2

--- build/version.h
+++ build/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,16,8,1
-#define STR_FILE_VERSION   "2,16,8,1\0"
-#define STR_FILE_VERSION2  "2.16.8.1"
-#define STR_DATE           "14 Dec 2024\0"
+#define FILE_VERSION        2,16,8,2
+#define STR_FILE_VERSION   "2,16,8,2\0"
+#define STR_FILE_VERSION2  "2.16.8.2"
+#define STR_DATE           "17 Dec 2024\0"
 #define NOMFENETRE    "HxCFloppyEmulator v" STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
