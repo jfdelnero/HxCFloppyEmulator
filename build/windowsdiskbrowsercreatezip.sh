0e60382add33ba890d60d92d507166bfa1fea563
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2025-06-14 01:27:46 +0200

    copy only dosdiskbrowser exec.

--- build/windowsdiskbrowsercreatezip.sh
+++ build/windowsdiskbrowsercreatezip.sh
@@ -3,6 +3,6 @@ mkdir HxCFloppyEmulator_DosDiskBrowser/Windows$1
 cp ../HxCFloppyEmulator_software/COPYING ./HxCFloppyEmulator_DosDiskBrowser/ || exit 1
 cp ../HxCFloppyEmulator_software/COPYING_FULL ./HxCFloppyEmulator_DosDiskBrowser/ || exit 1
 cp libhxcfe.dll ./HxCFloppyEmulator_DosDiskBrowser/Windows$1/ || exit 1
-cp *.exe ./HxCFloppyEmulator_DosDiskBrowser/Windows$1/ || exit 1
+cp DosDiskBrowser.exe ./HxCFloppyEmulator_DosDiskBrowser/Windows$1/ || exit 1
 
 zip -r HxCFloppyEmulator_DosDiskBrowser_win$1.zip HxCFloppyEmulator_DosDiskBrowser/
