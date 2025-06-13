1263cf15f017a4fde42e7051f7e18562ef61e4ec
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2025-06-14 01:20:44 +0200

    fix path.

--- build/windowsdiskbrowsercreatezip.sh
+++ build/windowsdiskbrowsercreatezip.sh
@@ -1,7 +1,7 @@
 mkdir HxCFloppyEmulator_DosDiskBrowser
 mkdir HxCFloppyEmulator_DosDiskBrowser/Windows$1
-cp ../HxCFloppyEmulator_DosDiskBrowser/COPYING ./HxCFloppyEmulator_DosDiskBrowser/ || exit 1
-cp ../HxCFloppyEmulator_DosDiskBrowser/COPYING_FULL ./HxCFloppyEmulator_DosDiskBrowser/ || exit 1
+cp ../HxCFloppyEmulator_software/COPYING ./HxCFloppyEmulator_DosDiskBrowser/ || exit 1
+cp ../HxCFloppyEmulator_software/COPYING_FULL ./HxCFloppyEmulator_DosDiskBrowser/ || exit 1
 cp libhxcfe.dll ./HxCFloppyEmulator_DosDiskBrowser/Windows$1/ || exit 1
 cp *.exe ./HxCFloppyEmulator_DosDiskBrowser/Windows$1/ || exit 1
 
