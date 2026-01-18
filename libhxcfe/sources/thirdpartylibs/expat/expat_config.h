9d5ac080d2a91ce5a28e9ccd8f011c9dfa2b64c7
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2026-01-18 19:26:16 +0100

    expat 2.7.3

--- libhxcfe/sources/thirdpartylibs/expat/expat_config.h
+++ libhxcfe/sources/thirdpartylibs/expat/expat_config.h
@@ -1,105 +1,106 @@
+/* expat_config.h.  Generated from expat_config.h.in by configure.  */
 /* expat_config.h.in.  Generated from configure.ac by autoheader.  */
 
 #ifndef EXPAT_CONFIG_H
 #define EXPAT_CONFIG_H 1
 
 /* Define if building universal (internal helper macro) */
-#undef AC_APPLE_UNIVERSAL_BUILD
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
 
 /* 1234 = LILENDIAN, 4321 = BIGENDIAN */
-#undef BYTEORDER
+#define BYTEORDER 1234
 
 /* Define to 1 if you have the `arc4random' function. */
-#undef HAVE_ARC4RANDOM
+/* #undef HAVE_ARC4RANDOM */
 
 /* Define to 1 if you have the `arc4random_buf' function. */
-#undef HAVE_ARC4RANDOM_BUF
+#define HAVE_ARC4RANDOM_BUF 1
 
 /* define if the compiler supports basic C++11 syntax */
-#undef HAVE_CXX11
+#define HAVE_CXX11 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#undef HAVE_DLFCN_H
+#define HAVE_DLFCN_H 1
 
 /* Define to 1 if you have the <fcntl.h> header file. */
-#undef HAVE_FCNTL_H
+#define HAVE_FCNTL_H 1
 
 /* Define to 1 if you have the 'getpagesize' function. */
-#undef HAVE_GETPAGESIZE
+#define HAVE_GETPAGESIZE 1
 
 /* Define to 1 if you have the `getrandom' function. */
-#undef HAVE_GETRANDOM
+#define HAVE_GETRANDOM 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the 'bsd' library (-lbsd). */
-#undef HAVE_LIBBSD
+/* #undef HAVE_LIBBSD */
 
 /* Define to 1 if you have a working 'mmap' system call. */
-#undef HAVE_MMAP
+#define HAVE_MMAP 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdio.h> header file. */
-#undef HAVE_STDIO_H
+#define HAVE_STDIO_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+#define HAVE_STRING_H 1
 
 /* Define to 1 if you have `syscall' and `SYS_getrandom'. */
-#undef HAVE_SYSCALL_GETRANDOM
+#define HAVE_SYSCALL_GETRANDOM 1
 
 /* Define to 1 if you have the <sys/param.h> header file. */
-#undef HAVE_SYS_PARAM_H
+#define HAVE_SYS_PARAM_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Define to the sub-directory where libtool stores uninstalled libraries. */
-#undef LT_OBJDIR
+#define LT_OBJDIR ".libs/"
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "expat"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT "https://github.com/libexpat/libexpat/issues"
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "expat"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "expat 2.7.3"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "expat"
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "2.7.3"
 
 /* Define to 1 if all of the C89 standard headers exist (not just the ones
    required in a freestanding environment). This macro is provided for
    backward compatibility; new code need not use it. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Version number of package */
-#undef VERSION
+#define VERSION "2.7.3"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
@@ -109,34 +110,34 @@
 # endif
 #else
 # ifndef WORDS_BIGENDIAN
-#  undef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
 # endif
 #endif
 
 /* Define to allow retrieving the byte offsets for attribute names and values.
    */
-#undef XML_ATTR_INFO
+/* #undef XML_ATTR_INFO */
 
 /* Define to specify how much context to retain around the current parse
    point, 0 to disable. */
-#undef XML_CONTEXT_BYTES
+#define XML_CONTEXT_BYTES 1024
 
 /* Define to include code reading entropy from `/dev/urandom'. */
-#undef XML_DEV_URANDOM
+#define XML_DEV_URANDOM 1
 
 /* Define to make parameter entity parsing functionality available. */
-#undef XML_DTD
+#define XML_DTD 1
 
 /* Define as 1/0 to enable/disable support for general entities. */
-#undef XML_GE
+#define XML_GE 1
 
 /* Define to make XML Namespaces functionality available. */
-#undef XML_NS
+#define XML_NS 1
 
 /* Define to empty if 'const' does not conform to ANSI C. */
-#undef const
+/* #undef const */
 
 /* Define to 'long int' if <sys/types.h> does not define. */
-#undef off_t
+/* #undef off_t */
 
 #endif // ndef EXPAT_CONFIG_H
