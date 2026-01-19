4af7e094d6783205d497185a637af7b95cb7abc7
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2026-01-19 23:09:33 +0100

    Don't cache fat table : Fix issues with damaged / protected disks.

--- libhxcfe/sources/thirdpartylibs/FATIOlib/fat_opts.h
+++ libhxcfe/sources/thirdpartylibs/FATIOlib/fat_opts.h
@@ -28,7 +28,7 @@
 
 // Number of sectors per FAT_BUFFER (min 1)
 #ifndef FAT_BUFFER_SECTORS
-    #define FAT_BUFFER_SECTORS              128
+    #define FAT_BUFFER_SECTORS              1
 #endif
 
 // Max FAT sectors to buffer (min 1)
