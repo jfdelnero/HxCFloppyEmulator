70c8fb0b41cc687b2ab0c1918dc19191ffc9bb16
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2026-02-10 23:09:49 +0100

    Videobit Sistema S80 raw image xml description.

--- libhxcfe/sources/xml_disk/DiskLayouts/LayoutsIndex.h
+++ libhxcfe/sources/xml_disk/DiskLayouts/LayoutsIndex.h
@@ -72,6 +72,7 @@
 #include "data_DiskLayout_Sharp_X68000_2HD_1232KB_xml.h"
 #include "data_DiskLayout_Alesis_DataDisk_DD_800KB_xml.h"
 #include "data_DiskLayout_Analog_Devices_LTS_2020_xml.h"
+#include "data_DiskLayout_Videobit_Sistema_S80_SD_100KB_xml.h"
 
 datatype * disklayout_list[]=
 {
@@ -149,6 +150,7 @@ datatype * disklayout_list[]=
 	data_DiskLayout_Unitel_Videotex_xml,
 	data_DiskLayout_Sharp_X68000_2HD_1232KB_xml,
 	data_DiskLayout_Analog_Devices_LTS_2020_xml,
+	data_DiskLayout_Videobit_Sistema_S80_SD_100KB_xml,
 	0
 };
 
